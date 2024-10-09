03ac069ac24ba4d222baf3e25fcc9652dc948d44
Amir Adel <amiradel1234567@gmail.com>
2024-10-09 04:55:10 +0300

    Updating post request route of health data to include newly added columns

--- back-end/routes/healthRoutes/postHealthRoute.js
+++ back-end/routes/healthRoutes/postHealthRoute.js
@@ -1,39 +1,56 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
 
+router.post("/health", async (req, res) => {
+  try {
+    const {
+      userId,
+      waterAccess,
+      waterQuality,
+      diseaseRisk,
+      healthNotes,
+      sanitationAccess,
+      diseaseOutbreakId,
+      submitterName,
+      submitterEmail,
+      submitterRole,
+      createdAt,
+      consentGranted,
+      fetchedAt,
+      timestamp,
+    } = req.body;
 
+    // Log request body for debugging
+    console.log("Request Body: ", req.body);
 
-router.post('/health', async (req, res) => {
-    try {
-      const { userId, waterAccess, waterQuality, diseaseRisk, healthNotes,  sanitationAccess, diseaseOutbreakId, submitterName, submitterEmail, submitterRole, createdAt } = req.body;
-      
-      // Log request body for debugging
-      console.log("Request Body: ", req.body);
-      
-      const newhealthData = await prisma.healthData.create({
-        data: {
-          userId,
-          waterAccess,
-          waterQuality,
-          diseaseRisk,
-          healthNotes,
-          sanitationAccess,
-          diseaseOutbreakId,
-          submitterName,
-          submitterEmail,
-          submitterRole,
-          createdAt,
-        },
-      });
-      
-      res.status(201).json(newhealthData);
-    } catch (error) {
-      console.error("Error inserting health data: ", error);
-      res.status(500).json({ error: 'Failed to insert health data', details: error.message });
-    }
-  });
-  
+    const newhealthData = await prisma.healthData.create({
+      data: {
+        userId,
+        waterAccess,
+        waterQuality,
+        diseaseRisk,
+        healthNotes,
+        sanitationAccess,
+        diseaseOutbreakId,
+        submitterName,
+        submitterEmail,
+        submitterRole,
+        createdAt,
+        consentGranted,
+        fetchedAt,
+        timestamp,
+      },
+    });
+
+    res.status(201).json(newhealthData);
+  } catch (error) {
+    console.error("Error inserting health data: ", error);
+    res
+      .status(500)
+      .json({ error: "Failed to insert health data", details: error.message });
+  }
+});
 
 module.exports = router;
