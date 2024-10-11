1d6f6ebb2d9b9225a2a61df0ba4738a5668273c0
Amir Adel <amiradel1234567@gmail.com>
2024-10-11 23:23:43 +0300

    Usin all made pages in index.js -General pages are nested under root-layout -Needing authentication pges are nested under dashboardLayout

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+// Import the layouts
+import DashboardLayout from "./layouts/dashboard-layout";
+import RootLayout from "./layouts/root-layout";
+
+import { UsereProvider } from "./context/UserContext";
+import HomePage from "./pages/General/HomePage";
+import AboutPage from "./pages/General/AboutPage";
+import ContactPage from "./pages/General/ContactPage";
+import SignInPage from "./pages/General/SignInPage";
+import SignUpPage from "./pages/General/SignUpPage";
+import ProfilePage from "./pages/ProfilePage"
+
+
+
+
+// Set up the router
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      // Public Routes (no need for sign up or sign in)
+      { path: "/", element: <HomePage /> }, // Root-level route
+      { path: "/contact", element: <ContactPage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/sign-in/*", element: <SignInPage /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
+
+      // Dashboard Layout with nested routes (needing login)
+      {
+        path: "/dashboard",
+        element: <DashboardLayout />,
+        children: [
+          { path: "home", element: <HomePage /> },
+          { path: 'profile', element: <ProfilePage /> },  // Accessible as /dashboard/profile
+        ],
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
