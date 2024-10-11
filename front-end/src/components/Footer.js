65f5affbc80a78d568d1e95ee1a07555ec56c54c
Amir Adel <amiradel1234567@gmail.com>
2024-10-11 23:13:14 +0300

    Making footer reusable component

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <p>© 2024 Myanmar Flood Platform. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
