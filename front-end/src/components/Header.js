e6e541af66047c8354672c596891771eec0d55d2
Amir Adel <amiradel1234567@gmail.com>
2024-10-11 23:12:36 +0300

    Making Header reusable component -it includes different routes that will be modified and enhances later on -it force user to login or sign up if not yet

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,31 @@
+import {  SignOutButton, useAuth } from '@clerk/clerk-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  const {isSignedIn} = useAuth()
+  const classes = 'px-4 py-2 hover:text-black hover:underline hover:bg-blue-300 rounded-xl'
+  return (
+    <div className='flex items-center justify-center mt-4 text-primary-dark'>
+      <ul className='flex  items-center gap-8 mt-6 text-primary text-xl '>
+          <li className={classes}><Link to={"/dashboard/profile"}>Profile</Link></li>
+          <li className={classes}><Link to={"/about"}>About</Link></li>
+          <li className={classes}><Link to={"/contact"}>Contact</Link></li>
+          {isSignedIn && (
+            <SignOutButton className={classes}/>
+          )}
+
+          {!isSignedIn && (
+            <>
+                      <li className={classes}><Link to={"/sign-in"}>Sign In</Link></li>
+                      <li className={classes}><Link to={"/sign-up"}>Sign Up</Link></li>
+            </>
+          )}
+
+
+        </ul>
+    </div>
+  )
+}
+
+export default Header
