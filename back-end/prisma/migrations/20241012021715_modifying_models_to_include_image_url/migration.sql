06d8dfe7f60dac3eebc47a49fc0e25266f94408c
Amir Adel <amiradel1234567@gmail.com>
2024-10-12 05:18:05 +0300

    Applying modifying-models-to-include-image-url migration to actual db

---
+++ back-end/prisma/migrations/20241012021715_modifying_models_to_include_image_url/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "DecisionMaker" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "HighLevelOrganization" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "Organization" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "OrganizationMember" ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "imageUrl" TEXT;
