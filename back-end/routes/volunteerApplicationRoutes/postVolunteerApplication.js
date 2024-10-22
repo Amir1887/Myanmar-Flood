be62d5682dcb1b7b4234d7eb95a2444ba4f47f6e
Amir Adel <amiradel1234567@gmail.com>
2024-10-22 11:50:58 +0300

    Updating existing records of volunteering applications

--- back-end/routes/volunteerApplicationRoutes/postVolunteerApplication.js
+++ back-end/routes/volunteerApplicationRoutes/postVolunteerApplication.js
@@ -51,4 +51,38 @@ router.post("/volunteer-application", async (req, res) => {
   }
 });
 
+
+router.post("/volunteer-application/:appId", async (req, res) => {
+  try {
+    // Extracting the volunteer application data from the request body
+    const { appId } = req.params;
+
+    const {
+      status,
+      reviewedAt,
+      organizationId,
+    } = req.body;
+
+    // Update the existing volunteer application record in the database
+    const volunteerApplicationData = await prisma.volunteerApplication.update({
+      where: {
+        id: parseInt(appId),  // Ensure appId is parsed as an integer
+      },
+      data: {
+        status,
+        reviewedAt,
+        organizationId,
+      },
+    });
+
+    res.status(201).json(volunteerApplicationData);
+  } catch (error) {
+    console.error("Error updating volunteer application status: ", error);
+    res.status(500).json({
+      error: "Failed to update volunteer application status",
+      details: error.message,
+    });
+  }
+});
+
 module.exports = router;
