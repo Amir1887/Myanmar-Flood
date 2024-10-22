625c56f59ceca3809014f62af4c05dc1f173ba96
Amir Adel <amiradel1234567@gmail.com>
2024-10-22 10:28:26 +0300

    retrieing all the data from backend and displaying it in volunteering (org side)

--- front-end/src/pages/Volunteering/VolunteeringToOrgSide.js
+++ front-end/src/pages/Volunteering/VolunteeringToOrgSide.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function VolunteeringToOrgSide() {
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    const getApplication = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/volunteer-application`
+        );
+        console.log("all app.....", response.data); // Combined application + user data
+        if (response.data) {
+          setApplications(response.data);
+        }
+      } catch (error) {
+        console.error("Error fetching the volunteer application:", error);
+      }
+    };
+
+    getApplication();
+  }, []);
+
   return (
     <div>
-      volunteering to org side
+      <h1>Volunteering Applications</h1>
+      {applications.map((app) => (
+        <div key={app.id}>
+          <p>Application ID: {app.id}</p>
+          <p>Applicant Name: {app.user.name}</p> {/* Access user details */}
+          <p>Location: {app.location}</p>
+          <p>Skills: {app.skills}</p>
+          <p>Status: {app.status}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default VolunteeringToOrgSide
+export default VolunteeringToOrgSide;
