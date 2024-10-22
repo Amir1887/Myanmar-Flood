c6865b375eef920c9a557d62dd23b5d8f0ac0af9
Amir Adel <amiradel1234567@gmail.com>
2024-10-22 11:57:03 +0300

    Including org in get request to display on userSide

--- back-end/routes/volunteerApplicationRoutes/getVolunteerApplication.js
+++ back-end/routes/volunteerApplicationRoutes/getVolunteerApplication.js
@@ -1,20 +1,20 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
 
-
-
 //getting all applications of all users
-router.get('/volunteer-application', async (req, res) => {
+router.get("/volunteer-application", async (req, res) => {
   try {
     // Use Prisma's `include` feature to include user data
-    const volunteerApplicationData = await prisma.volunteerApplication.findMany({
-      include: {
-        user: true, // This will include user data in the response
-        organization:true,
-      },
-    });
+    const volunteerApplicationData = await prisma.volunteerApplication.findMany(
+      {
+        include: {
+          user: true, // This will include user data in the response
+          organization: true,
+        },
+      }
+    );
     console.log("all app backend", volunteerApplicationData);
     res.json(volunteerApplicationData);
   } catch (error) {
@@ -23,27 +23,37 @@ router.get('/volunteer-application', async (req, res) => {
   }
 });
 
-
 // Getting a specific user's application based on userId
-router.get('/volunteer-application/:userId', async (req, res) => {
+router.get("/volunteer-application/:userId", async (req, res) => {
   const { userId } = req.params;
-  
+
   try {
-    const volunteerApplicationData = await prisma.volunteerApplication.findUnique({
-      where: {
-        userId: parseInt(userId), // Ensure the userId is an integer
-      },
-    });
-    
+    const volunteerApplicationData =
+      await prisma.volunteerApplication.findUnique({
+        where: {
+          userId: parseInt(userId), // Ensure the userId is an integer
+        },
+        include: {
+          organization: true,
+        },
+      });
+
     if (volunteerApplicationData) {
       res.json(volunteerApplicationData);
-      console.log("Volunteer application data for user:", volunteerApplicationData);
+      console.log(
+        "Volunteer application data for user:",
+        volunteerApplicationData
+      );
     } else {
-      res.status(404).json({ message: "No volunteer application found for this user." });
+      res
+        .status(404)
+        .json({ message: "No volunteer application found for this user." });
     }
   } catch (error) {
     console.error("Error fetching volunteer application for user:", error);
-    res.status(500).json({ error: "Failed to fetch volunteer application for user." });
+    res
+      .status(500)
+      .json({ error: "Failed to fetch volunteer application for user." });
   }
 });
 module.exports = router;
