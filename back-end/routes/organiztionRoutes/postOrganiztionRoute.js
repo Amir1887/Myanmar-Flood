514bacbce1b513978c5fe389130dd92a8c408112
Amir Adel <amiradel1234567@gmail.com>
2024-10-29 20:27:27 +0300

    Modifying route to include newly added org data

--- back-end/routes/organiztionRoutes/postOrganiztionRoute.js
+++ back-end/routes/organiztionRoutes/postOrganiztionRoute.js
@@ -1,32 +1,49 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
 
+router.post("/organiztions", async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      createdAt,
+      imageUrl,
+      username,
+      highLevelOrg,
+      location,
+      phoneNumber,
+      password,
+    } = req.body;
 
+    // Log request body for debugging
+    console.log("Request Body From org route: ", req.body);
 
-router.post('/organiztions', async (req, res) => {
-    try {
-      const { name, contactInfo, location, createdAt } = req.body;
-      
-      // Log request body for debugging
-      console.log("Request Body: ", req.body);
-      
-      const newOrganiztionData = await prisma.organization.create({
-        data: {
-          name,
-          contactInfo,
-          location,
-          createdAt,
-        },
+    const newOrganiztionData = await prisma.organization.create({
+      data: {
+        name,
+        email,
+        createdAt,
+        imageUrl,
+        username,
+        RelatedhighLevelOrg: highLevelOrg,
+        location,
+        phoneNumber,
+        passwordEnabled: password,
+      },
+    });
+
+    res.status(201).json(newOrganiztionData);
+  } catch (error) {
+    console.error("Error inserting organiztion data: ", error);
+    res
+      .status(500)
+      .json({
+        error: "Failed to insert organiztion data",
+        details: error.message,
       });
-      
-      res.status(201).json(newOrganiztionData);
-    } catch (error) {
-      console.error("Error inserting organiztion data: ", error);
-      res.status(500).json({ error: 'Failed to insert organiztion data', details: error.message });
-    }
-  });
-  
+  }
+});
 
 module.exports = router;
