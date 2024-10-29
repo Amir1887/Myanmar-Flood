cdc260b66f72d496dde45ce6d12638737b870174
Amir Adel <amiradel1234567@gmail.com>
2024-10-29 15:48:27 +0300

    Enhance styling making header at the center of the page

--- front-end/src/pages/General/HomePage.js
+++ front-end/src/pages/General/HomePage.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Header from '../../components/Header'
+import React from 'react';
+import Header from '../../components/Header';
 
 function HomePage() {
   return (
-    <div className='flex flex-col items-center justify-center mt-4 text-primary-dark'>
-       <h1 className="text-4xl font-bold mb-6">Welcome to Myanmar Flood Platform</h1>
-      <Header/>
+    <div className="flex flex-col items-center justify-center text-primary-dark h-screen">
+      {/* Wrapper with background image */}
+      <div
+        className="relative w-full h-full bg-cover bg-center flex flex-col items-center text-white"
+        style={{
+          backgroundImage: `url('https://images.pexels.com/photos/11884556/pexels-photo-11884556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+        }}
+      >
+        {/* Header with adjusted spacing from the top */}
+        <div className="mt-8 w-full flex justify-center">
+          <Header />
+        </div>
+
+        {/* Title with margin to space it below the navbar */}
+        <h1 className="text-4xl font-bold mb-6 bg-black bg-opacity-60 p-4 rounded-md mt-20">
+          Welcome to Myanmar Flood Platform
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
