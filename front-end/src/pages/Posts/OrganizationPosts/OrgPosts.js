af1ac3efb753ca32a95af0c1a5b6e4df0ca985b9
Amir Adel <amiradel1234567@gmail.com>
2024-10-30 13:41:01 +0300

    Sharing props to org side and dispalying input and buttons for previewing photo

--- front-end/src/pages/Posts/OrganizationPosts/OrgPosts.js
+++ front-end/src/pages/Posts/OrganizationPosts/OrgPosts.js
@@ -1,11 +1,49 @@
 import React from 'react'
 
-function OrgPosts() {
-  return (
-    <div>
-      org posts
-    </div>
-  )
+function OrgPosts({file, uploading, preview, onFileChange, setUploading}) {
+
+    //upload photo
+    const onUpload = async () => {
+
+
+    };
+    return (
+      <div>
+        <h1>Organization posts</h1>
+        <input type="text" placeholder="what updates you want to share?" required/>
+        <div className="mb-4">
+          <label
+            htmlFor="file-upload"
+            className="cursor-pointer inline-block p-3 text-white bg-blue-500 rounded-xl font-semibold hover:bg-blue-600"
+          >
+            {preview ? "Change Photo" : "Choose Photo"}
+          </label>
+          <input
+            id="file-upload"
+            type="file"
+            className="hidden" // Hide the default file input
+            onChange={onFileChange}
+          />
+        </div>
+  
+      {preview && (
+              <div>
+              <div className="mb-4 ml-5">
+                <h3>Image Preview:</h3>
+                <img src={preview} alt="Preview" style={{ width: "300px" }} />
+              </div>
+      
+              <button
+                className=" border font-semibold hover:border-blue-400 p-3 rounded-xl mb-4 max-w-xs"
+                onClick={onUpload}
+                disabled={uploading}
+              >
+                {uploading ? "Uploading..." : "Upload Photo"}
+              </button>
+            </div>
+      )}
+      </div>
+    );
 }
 
 export default OrgPosts
