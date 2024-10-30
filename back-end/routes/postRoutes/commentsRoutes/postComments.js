e9881ae9fb3c6011da363982e4f1e20ddbb55e06
Amir Adel <amiradel1234567@gmail.com>
2024-10-31 02:22:19 +0300

    Separating routes for better handling, each comment should be inserted in its proper place

--- back-end/routes/postRoutes/commentsRoutes/postComments.js
+++ back-end/routes/postRoutes/commentsRoutes/postComments.js
@@ -3,10 +3,10 @@ const prisma = new PrismaClient();
 const express = require("express");
 const router = express.Router();
 
-router.post("/comments", async (req, res) => {
+router.post("/comments-org", async (req, res) => {
   try {
     const {
-        content, createdAt, postId, userId, organizationId
+      commentInfo, PostId, orgId
     } = req.body;
 
     // Log request body for debugging
@@ -14,11 +14,38 @@ router.post("/comments", async (req, res) => {
 
     const newCommentData = await prisma.comment.create({
       data: {
-        content, createdAt, postId, userId, organizationId
+        content: commentInfo, 
+        postId: PostId, 
+        organizationId: orgId
       },
     });
 
     res.status(201).json(newCommentData);
+  } catch (error) {
+    console.error("Error inserting new comment Data: ", error);
+    res.status(500).json({
+      error: "Failed to insert commetn data ",
+      details: error.message,
+    });
+  }
+});
+
+router.post("/comments-user", async (req, res) => {
+  try {
+    const {
+        content, createdAt, postId, userId
+    } = req.body;
+
+    // Log request body for debugging
+    console.log("Request Body From comments: ", req.body);
+
+    // const newCommentData = await prisma.comment.create({
+    //   data: {
+    //     content, createdAt, postId, userId, organizationId
+    //   },
+    // });
+
+    // res.status(201).json(newCommentData);
   } catch (error) {
     console.error("Error inserting new comment Data: ", error);
     res.status(500).json({
