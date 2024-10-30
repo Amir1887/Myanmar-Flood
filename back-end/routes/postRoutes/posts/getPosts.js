4bef908b7b6fdee6e7ffaeb8e01a4ebe2af77db2
Amir Adel <amiradel1234567@gmail.com>
2024-10-30 23:20:31 +0300

    Joining comments and likes related to post

--- back-end/routes/postRoutes/posts/getPosts.js
+++ back-end/routes/postRoutes/posts/getPosts.js
@@ -7,7 +7,12 @@ const router = express.Router();
 
 
 router.get('/posts', async (req, res) => {
-  const postData = await prisma.post.findMany();
+  const postData = await prisma.post.findMany({
+    include: {
+      comments: true, // Include all comments related to each post
+      likes: true     // Include all likes related to each post
+    }
+  });
   res.json(postData);
   console.log(" post data", postData);
 });
